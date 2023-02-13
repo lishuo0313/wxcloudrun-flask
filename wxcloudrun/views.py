8d6aeb5f4a16247c6872e6448a2f94a3e389fbaf
lishuo0313 <125233289+lishuo0313@users.noreply.github.com>
2023-02-13 11:21:34 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,66 +1,37 @@
-from datetime import datetime
-from flask import render_template, request
-from run import app
-from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
-from wxcloudrun.model import Counters
-from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+import werobot
+import openai
+
+openai.api_key = "你的api_key"
+
+robot = werobot.WeRoBot()
+
+class RobotConfig(object):
+    HOST="0.0.0.0"
+    PORT= 80
+    TOKEN = "you token"# token是微信公众号用来指定接入当前云服务器的服务的凭证，代表是自己人接入的，等一下就有什么用了
+    
+robot.config.from_object(RobotConfig)
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.7,
+    max_tokens=3000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    message = response.choices[0].text
+    return message.strip()
 
 
-@app.route('/')
-def index():
-    """
-    :return: 返回index页面
-    """
-    return render_template('index.html')
+
+@robot.handler
+def hello (messages):
+    print(messages.content)
+    return generate_response(messages.content)
 
 
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
-
-    # 获取请求体参数
-    params = request.get_json()
-
-    # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
-
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
-
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
-
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
-
-
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+if __name__ == "__main__":
+    robot.run()
